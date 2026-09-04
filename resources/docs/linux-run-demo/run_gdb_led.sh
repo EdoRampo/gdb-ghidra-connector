74fd4e69b6004a4e603cdb574cbeaa582fff31a4
YangQing <sunking@YQ-RedMi.localdomain>
2026-09-04 12:31:43 +0800

    add Ghidra Host Parameter

--- resources/docs/linux-run-demo/run_gdb_led.sh
+++ resources/docs/linux-run-demo/run_gdb_led.sh
@@ -4,14 +4,16 @@
 MODE=gdb
 BACKEND_HOST=localhost
 BACKEND_PORT=1234
+GHIDRA_HOST=auto
 GHIDRA_PORT=18932    
-while getopts "m:H:p:g:h" opt; do
+while getopts "m:H:p:G:g:h" opt; do
     case "$opt" in
         m) MODE="$OPTARG" ;;
         H) BACKEND_HOST="$OPTARG" ;;
         p) BACKEND_PORT="$OPTARG" ;;
+        G) GHIDRA_HOST="$OPTARG" ;;
         g) GHIDRA_PORT="$OPTARG" ;;
-        h) echo "用法: $0 -m <gdb|ghidra> -H <backend_host> -p <backend_port> -g <ghidra_port>"; exit 0 ;;
+        h) echo "用法: $0 -m <gdb|ghidra> -H <backend_host> -p <backend_port> -G <ghidra_host> -g <ghidra_port>"; exit 0 ;;
         *) exit 1 ;;
     esac
 done
@@ -23,7 +25,7 @@ then
 elif [[ "$MODE" == "ghidra" ]]
 then
     ./src/gdb_ghidra_connect.sh -a arm -f ./resources/samples/others/nucleo_fre411_gpio_led.axf -H $BACKEND_HOST -p $BACKEND_PORT \
-    -g /path/to/ghidra_12.0.4_PUBLIC -G auto:$GHIDRA_PORT -V .venv
+    -g /path/to/ghidra_12.0.4_PUBLIC -G $GHIDRA_HOST:$GHIDRA_PORT -V .venv
 else
     echo "invalid mode: $MODE, please choose gdb or ghidra"
     exit 1
